2aa4cd3c2a814fcd8cadc352afd0492cdc6f0ab4
Amirttrima <amirsrst@gmail.com>
2024-07-19 14:14:56 +0330

    JOS v0.2 classes completed.

--- job_offer_system/main.py
+++ job_offer_system/main.py
@@ -1,173 +1,263 @@
 """
 Job Offer System
 """
-
 from enum import Enum
 
+class JobError(Exception):
+    """Base class for exceptions in this module."""
+
+class FullNameError(JobError):
+    """Exception raised for invalid names."""
+    def __init__(self, message="Invalid name"):
+        self.message = message
+        super().__init__(self.message)
+
+class AgeError(JobError):
+    """Exception raised for invalid age."""
+    def __init__(self, message="Invalid age"):
+        self.message = message
+        super().__init__(self.message)
+
+class TimeConditionError(JobError):
+    """Exception raised for invalid time conditions."""
+    def __init__(self, message="Invalid time condition"):
+        self.message = message
+        super().__init__(self.message)
+
+class SalaryError(JobError):
+    """Exception raised for invalid salary."""
+    def __init__(self, message="Invalid salary"):
+        self.message = message
+        super().__init__(self.message)
+
 class TimeCondition(Enum):
-    """
-    Time Condition Enum
-    """
+    """Time Condition Enum"""
     FULLTIME = "FULLTIME"
     PARTTIME = "PARTTIME"
     PROJECT = "PROJECT"
 
 class Job:
-    """
-    Job Class
-    """
+    """Job Class"""
+    _id_counter = 1
+
     def __init__(self) -> None:
-        self.skill  = []
-        self.name = ''
-        self.min_age = 18
-        self.max_age = 60
-        self.time_cond = TimeCondition
-        self.salary = 1000
+        self.id = Job._id_counter
+        Job._id_counter += 1
+        self._skills = []
+        self._name = ''
+        self._min_age = 18
+        self._max_age = 60
+        self._time_condition = TimeCondition.FULLTIME  # Default time condition
+        self._salary = 1000
 
     # Properties
 
     @property
-    def skill(self):
-        """
-        skill property
-        """
-        return self.skill
+    def skills(self):
+        """Skills property."""
+        return self._skills
 
     @property
     def name(self):
-        """
-        name property
-        """
-        return self.name
+        """Name property."""
+        return self._name
 
     @property
     def min_age(self):
-        """
-        min_age property
-        """
-        return self.min_age
+        """Minimum age property."""
+        return self._min_age
 
     @property
     def max_age(self):
-        """
-        max_age property
-        """
-        return self.max_age
+        """Maximum age property."""
+        return self._max_age
 
     @property
-    def time_cond(self):
-        """
-        time_cond property
-        """
-        return self.time_cond
+    def time_condition(self):
+        """Time condition property."""
+        return self._time_condition
 
     @property
     def salary(self):
-        """
-        salary property
-        """
-        return self.salary
+        """Salary property."""
+        return self._salary
 
     # Setters
 
-    @skill.setter
-    def skill(self, *skill):
-        self.skill.append(skill)
+    @skills.setter
+    def skills(self, *skills):
+        """Set skills property."""
+        self._skills.extend(skills)
 
     @name.setter
     def name(self, name):
-        if self.check_name(name):
-            self.name = name
+        """Set name property."""
+        if self._check_name(name):
+            self._name = name
         else:
-            # Name Error Exception
-            pass
+            raise FullNameError()
 
     @min_age.setter
-    def min_age(self, min_age, max_age):
-        if self.check_age(min_age, max_age):
-            self.min_age = min_age
+    def min_age(self, min_age):
+        """Set minimum age property."""
+        if self._check_age(min_age, self._max_age):
+            self._min_age = min_age
         else:
-            # Age Violation Exception
-            pass
+            raise AgeError()
 
     @max_age.setter
-    def max_age(self, min_age, max_age):
-        if self.check_age(min_age, max_age):
-            self.max_age = max_age
+    def max_age(self, max_age):
+        """Set maximum age property."""
+        if self._check_age(self._min_age, max_age):
+            self._max_age = max_age
         else:
-            # Age Violation Exception
-            pass
+            raise AgeError()
 
-    @time_cond.setter
-    def time_cond(self, cond):
-        if self.check_time_cond(cond):
-            self.time_cond = cond
+    @time_condition.setter
+    def time_condition(self, condition):
+        """Set time condition property."""
+        if self._check_time_condition(condition):
+            self._time_condition = condition
         else:
-            # Time Condition Error
-            pass
+            raise TimeConditionError()
 
     @salary.setter
     def salary(self, salary):
-        if self.check_salary(salary):
-            self.salary = salary
+        """Set salary property."""
+        if self._check_salary(salary):
+            self._salary = salary
         else:
-            # Salary Amount Exception
-            pass
+            raise SalaryError()
 
-    def check_name(self, name):
-        """
-        to check whether the name is correct
-        """
+    def _check_name(self, name):
+        """Check whether the name is valid."""
         if not 1 <= len(name) <= 10:
             return False
         if not name.isalpha():
             return False
         return True
 
-
-    def check_age(self, min_age, max_age):
-        """
-        to check whether ages are correct
-        """
-        if (min_age < 0 or min_age > 200) and (max_age < 0 or max_age > 200):
+    def _check_age(self, min_age, max_age):
+        """Check whether ages are valid."""
+        if (min_age < 0 or min_age > 200) or (max_age < 0 or max_age > 200):
             return False
-        else:
-            if min_age > max_age:
-                return False
-            else:
-                return True
-
-
-    def check_time_cond(self, time_cond):
-        """
-        to check whether the time condition is correct
-        """
-        if (time_cond == TimeCondition.FULLTIME or
-            time_cond == TimeCondition.PARTTIME or
-            time_cond == TimeCondition.PROJECT):
-            return True
-        else:
+        if min_age > max_age:
             return False
+        return True
 
-    def check_salary(self, salary):
-        """
-        to check whether the salary is correct
-        """
-        if salary < 0:
-            return False
-        if salary >= 1_000_000_000:
-            return False
-        if salary % 1000 != 0:
+    def _check_time_condition(self, time_condition):
+        """Check whether the time condition is valid."""
+        return time_condition in (TimeCondition.FULLTIME,
+                                   TimeCondition.PARTTIME,
+                                   TimeCondition.PROJECT)
+
+    def _check_salary(self, salary):
+        """Check whether the salary is valid."""
+        if salary < 0 or salary >= 1_000_000_000 or salary % 1000 != 0:
             return False
         return True
 
 class User:
-    """
-    User Class
-    """
+    """User Class"""
+    _id_counter = 1
+
     def __init__(self) -> None:
-        self.skill = []
-        self.name = ''
-        self.age = 0
-        self.time_cond = ['full', 'part', 'proj']
-        self.salary = 1000
+        self.id = User._id_counter
+        User._id_counter += 1
+        self._skills = []
+        self._name = ''  # Initialize _name directly
+        self._age = 0  # Initialize _age directly
+        self._time_condition = 'FULLTIME'  # Initialize _time_condition directly
+        self._salary = 1000  # Initialize _salary directly
+
+    # Properties
+
+    @property
+    def skills(self):
+        """Skills property."""
+        return self._skills
+
+    @property
+    def name(self):
+        """Name property."""
+        return self._name
+
+    @property
+    def age(self):
+        """Age property."""
+        return self._age
+
+    @property
+    def time_condition(self):
+        """Time condition property."""
+        return self._time_condition
+
+    @property
+    def salary(self):
+        """Salary property."""
+        return self._salary
+
+    # Setters
+
+    @skills.setter
+    def skills(self, *skills):
+        """Set skills property."""
+        self._skills.extend(skills)
+
+    @name.setter
+    def name(self, name):
+        """Set name property."""
+        if self._check_name(name):
+            self._name = name
+        else:
+            raise FullNameError()
+
+    @age.setter
+    def age(self, age):
+        """Set age property."""
+        if self._check_age(age):
+            self._age = age
+        else:
+            raise AgeError()
+
+    @time_condition.setter
+    def time_condition(self, condition):
+        """Set time condition property."""
+        if self._check_time_condition(condition):
+            self._time_condition = condition
+        else:
+            raise TimeConditionError()
+
+    @salary.setter
+    def salary(self, salary):
+        """Set salary property."""
+        if self._check_salary(salary):
+            self._salary = salary
+        else:
+            raise SalaryError()
+
+    def _check_name(self, name):
+        """Check whether the name is valid."""
+        if not 1 <= len(name) <= 10:
+            return False
+        if not name.isalpha():
+            return False
+        return True
+
+    def _check_age(self, age):
+        """Check whether the age is valid."""
+        if age < 0 or age > 200:
+            return False
+        return True
+
+    def _check_time_condition(self, time_condition):
+        """Check whether the time condition is valid."""
+        return time_condition in (TimeCondition.FULLTIME,
+                                   TimeCondition.PARTTIME,
+                                   TimeCondition.PROJECT)
+
+    def _check_salary(self, salary):
+        """Check whether the salary is valid."""
+        if salary < 0 or salary >= 1_000_000_000 or salary % 1000 != 0:
+            return False
+        return True
---
+++ job_offer_system/tester.py
@@ -0,0 +1,68 @@
+"""
+a tester module
+"""
+from main import User, FullNameError, AgeError, TimeConditionError, SalaryError
+
+def test_user_class():
+    """
+    to test the user class
+    """
+    # Create a new user
+    user = User()
+
+    # Test default values
+    assert user.id == 1  # First user should have ID 1
+    assert user.name == ''  # Default name should be empty
+    assert user.age == 0  # Default age should be 0
+    assert user.time_condition == 'FULLTIME'  # Default time condition should be 'full'
+    assert user.salary == 1000  # Default salary should be 1000
+
+    # Test setting valid name
+    user.name = "Alice"
+    assert user.name == "Alice"
+
+    # Test setting invalid name (should raise FullNameError)
+    try:
+        user.name = "A" * 11  # Exceeds maximum length
+    except FullNameError:
+        print(FullNameError())
+
+    # Test setting valid age
+    user.age = 25
+    assert user.age == 25
+
+    # Test setting invalid age (should raise AgeError)
+    try:
+        user.age = -1  # Invalid age
+    except AgeError:
+        print(AgeError())
+
+    # Test setting valid time condition
+    user.time_condition = 'part'
+    assert user.time_condition == 'part'
+
+    # Test setting invalid time condition (should raise TimeConditionError)
+    try:
+        user.time_condition = 'invalid_condition'  # Not a valid condition
+    except TimeConditionError:
+        print(TimeConditionError())
+
+    # Test setting valid salary
+    user.salary = 5000
+    assert user.salary == 5000
+
+    # Test setting invalid salary (should raise SalaryError)
+    try:
+        user.salary = -1000  # Invalid salary
+    except SalaryError:
+        print(SalaryError())
+
+    # Test setting salary that is not a multiple of 1000 (should raise SalaryError)
+    try:
+        user.salary = 2500  # Not a valid salary
+    except SalaryError:
+        print(SalaryError())
+
+    print("All tests passed!")
+
+test_user_class()
